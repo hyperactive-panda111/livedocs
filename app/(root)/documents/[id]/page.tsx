686658daaae8d48bf61f58b568426271ee19342e
amomensah675 <amomensah675@gmail.com>
2024-07-24 04:43:15 +0000

    added editor to documents page

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,13 +1,11 @@
-import Header from '@/components/Header'
-import { Editor } from '@/components/editor/Editor'
-import { SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
+import CollaborativeRoom from '@/components/CollaborativeRoom'
 import React from 'react'
 
 const Documents = () => {
   return (
-    <div>
-     
-    </div>
+    <main className='flex w-full items-center flex-col'>
+      <CollaborativeRoom />
+    </main>
   )
 }
 
